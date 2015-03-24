ac7263c1f16a99ee8b503afec05282836a482ab4
Martin Lehmann <lehmar11@student.westerdals.no>
2015-03-24 12:14:04 +0100

    Show decibels (not working properly)

--- Office Analysis Sensor Thing/src/main/java/no/westerdals/officeanalysis/android/TemperatureActivity.java
+++ Office Analysis Sensor Thing/src/main/java/no/westerdals/officeanalysis/android/TemperatureActivity.java
@@ -1,19 +1,25 @@
 package no.westerdals.officeanalysis.android;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
-import org.androidannotations.annotations.Background;
-import org.androidannotations.annotations.Click;
-import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.ViewById;
+import android.widget.TextView;
+import org.androidannotations.annotations.*;
 import org.androidannotations.annotations.rest.RestService;
 
+import java.util.Arrays;
 import java.util.Date;
 
 @EActivity(R.layout.activity_temperature)
-public class TemperatureActivity extends Activity
+public class TemperatureActivity extends Activity implements SensorEventListener
 {
+    private final String TAG = TemperatureActivity.class.getSimpleName();
     @ViewById
     EditText serverIpEditText;
 
@@ -23,8 +29,99 @@ public class TemperatureActivity extends Activity
     @ViewById
     EditText integrationIdEditText;
 
+    @ViewById
+    TextView lightSensorValueTextField;
+
+    @ViewById
+    TextView proximitySensorValueTextField;
+
+    @ViewById
+    TextView decibelValueTextField;
+
+    private boolean getDecibels = false;
+
     @RestService
     TemperatureClient temperatureClient;
+
+    SensorManager sensorManager;
+
+    Sensor lightSensor;
+
+    Sensor proximitySensor;
+
+    @Bean
+    SoundMeter soundMeter;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        configureSoundMeter();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        sensorManager.unregisterListener(this);
+        getDecibels = false;
+        soundMeter.stop();
+    }
+
+    @AfterInject
+    void configureSoundMeter()
+    {
+        if (soundMeter != null)
+        {
+            getDecibels = true;
+            soundMeter.start();
+            getDecibelsForever();
+        }
+    }
+
+    @Background
+    void getDecibelsForever()
+    {
+        for (; ; )
+        {
+            if (!getDecibels)
+            {
+                break;
+            }
+
+            displayDecibels(soundMeter.getDecibels());
+
+            try
+            {
+                Thread.sleep(100);
+            }
+            catch (InterruptedException e)
+            {
+                Log.e(TAG, "Could not put decibels thread to sleep", e);
+            }
+        }
+    }
+
+    @UiThread
+    void displayDecibels(final double decibels)
+    {
+        decibelValueTextField.setText(String.valueOf(decibels));
+    }
 
     @Click(R.id.sendTemperatureButton)
     void prepareTemperatureSendoff()
@@ -39,7 +136,7 @@ public class TemperatureActivity extends Activity
 
         final Temperature temperature = new Temperature(temperatureString, timestamp);
 
-        Log.d(TemperatureActivity.class.getSimpleName(), "Prepared temperature sendoff. rootUrl = " + rootUrl + ", integrationId = " + integrationId + ", temperature = " + temperature);
+        Log.d(TAG, "Prepared temperature sendoff. rootUrl = " + rootUrl + ", integrationId = " + integrationId + ", temperature = " + temperature);
 
         sendTemperature(temperature, integrationId);
     }
@@ -47,8 +144,32 @@ public class TemperatureActivity extends Activity
     @Background
     void sendTemperature(Temperature temperature, String integrationId)
     {
-        Log.d(TemperatureActivity.class.getSimpleName(), "Sent temperature: " + temperature + " from integration id: " + integrationId);
+        Log.d(TAG, "Sent temperature: " + temperature + " from integration id: " + integrationId);
         String response = temperatureClient.send(temperature, integrationId);
-        Log.d(TemperatureActivity.class.getSimpleName(), "Got response: " + response);
+        Log.d(TAG, "Got response: " + response);
+    }
+
+    @Override
+    public void onSensorChanged(final SensorEvent sensorEvent)
+    {
+        Log.d(TAG, "onSensorChanged: " + Arrays.toString(sensorEvent.values));
+
+        if (sensorEvent.sensor == lightSensor)
+        {
+            lightSensorValueTextField.setText(String.valueOf(sensorEvent.values[0]));
+        }
+        else if (sensorEvent.sensor == proximitySensor)
+        {
+            proximitySensorValueTextField.setText(String.valueOf(sensorEvent.values[0]));
+        }
+        else
+        {
+            Log.d(TAG, "Some other sensor");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(final Sensor sensor, final int i)
+    {
     }
 }
